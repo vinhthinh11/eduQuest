e2f3536a3c1c0fa68e1880b309753fa2cc4dcbea
vinhthinh11 <vinhthinh1993@gmail.com>
2024-04-04 23:01:02 +0700

    add theme

---
+++ src/theme.js
@@ -0,0 +1,49 @@
+import { createContext, useState, useMemo } from 'react';
+import { createTheme } from '@mui/material';
+
+// de day co thoi gian ranh thi lam giao dien cho dark mode
+
+export const tokens = mode => ({});
+
+export const themeSetting = mode => {
+  const colors = tokens(mode);
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === 'dark'
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.secondary[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: '#fcfcfc',
+            },
+          }),
+    },
+  };
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
       textColor: { base: '#836FFF' },
       borderColor: { edu: '#836FFF' },
       colors: {
-        'text-default': 'rgb(131, 0, 0)',
         customPurple: 'rgb(131, 111, 225)',
         customPurpleLight: '#7355fd',
         'custom-purple': 'rgb(131, 111, 225)',
